0bfb8f904844327badcbdf627d0ae6fe40b3770d
sizwetshabangu <sizwetshabangu97@gmail.com>
2024-05-24 13:04:12 +0200

    scaffold project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { PublisherModule } from './publisher/publisher.module';
+import { CategoryModule } from './category/category.module';
+import { AddressModule } from './address/address.module';
+import { ReaderModule } from './reader/reader.module';
+import { StaffModule } from './staff/staff.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule, PublisherModule, CategoryModule, AddressModule, ReaderModule, StaffModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
