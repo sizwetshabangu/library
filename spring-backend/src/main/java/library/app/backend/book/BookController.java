becc832faa87e2f86e3281dae051525150d92005
sizwetshabangu <sizwetshabangu97@gmail.com>
2024-06-10 00:18:50 +0200

    Adding placeholder methods for book controller

--- spring-backend/src/main/java/library/app/backend/book/BookController.java
+++ spring-backend/src/main/java/library/app/backend/book/BookController.java
@@ -1,4 +1,37 @@
 package library.app.backend.book;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController()
+@RequestMapping("book")
 public class BookController {
+    public BookController() {}
+
+    @GetMapping()
+    public List<Book> get() {
+        return new ArrayList<Book>();
+    }
+
+    @GetMapping()
+    public Book getById() {
+        return new Book();
+    }
+
+    @PostMapping()
+    public Book post() {
+        return new Book();
+    }
+
+    @PutMapping()
+    public Book put() {
+        return new Book();
+    }
+
+    @DeleteMapping()
+    public Book deleteById() {
+        return new Book();
+    }
 }
