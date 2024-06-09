5cc083dad1fd4f968d2bcb50932cf9e75d1c72a0
sizwetshabangu <sizwetshabangu97@gmail.com>
2024-06-10 00:23:35 +0200

    Adding placeholder tests for CategoryControllerTest

---
+++ spring-backend/src/test/java/library/app/backend/category/CategoryControllerTest.java
@@ -0,0 +1,26 @@
+package library.app.backend.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CategoryControllerTest {
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void add() {
+    }
+}
