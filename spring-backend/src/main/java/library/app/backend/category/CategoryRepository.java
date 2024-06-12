7a0a7ef7eecaf7c88bbaaf13e6f3b650e656b68a
sizwetshabangu <sizwetshabangu97@gmail.com>
2024-06-12 21:23:28 +0200

    Remove query

--- spring-backend/src/main/java/library/app/backend/category/CategoryRepository.java
+++ spring-backend/src/main/java/library/app/backend/category/CategoryRepository.java
@@ -1,7 +1,6 @@
 package library.app.backend.category;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
